96a6c2def001754dc7fbe03a8bf8cb33ef59315d
Roman Syuzyov <rsyuzyov@gmail.com>
2025-11-15 13:55:25 +0500

    add utf locale

--- foreman-setup.sh
+++ foreman-setup.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 set -e
 
+# apt по умолчанию пытается работать на ipv6, но не всегда справляется
 grep -qxF "net.ipv6.conf.all.disable_ipv6 = 1" /etc/sysctl.conf || echo "net.ipv6.conf.all.disable_ipv6 = 1" | tee -a /etc/sysctl.conf
 grep -qxF "net.ipv6.conf.default.disable_ipv6 = 1" /etc/sysctl.conf || echo "net.ipv6.conf.default.disable_ipv6 = 1" | tee -a /etc/sysctl.conf
 grep -qxF "net.ipv6.conf.lo.disable_ipv6 = 1" /etc/sysctl.conf || echo "net.ipv6.conf.lo.disable_ipv6 = 1" | tee -a /etc/sysctl.conf
 
+# foreman требует в hosts запись для fqdn, если ip динамический
 grep -qxF "127.0.0.1 $(hostname -f) $(hostname -s)" /etc/hosts || echo "127.0.0.1 $(hostname -f) $(hostname -s)" >> /etc/hosts
 
 apt update && apt dist-upgrade -y
-apt install -y ca-certificates wget gnupg lsb-release
+apt install -y ca-certificates wget gnupg lsb-release locales
+
+sed -i 's/^# *\(en_US.UTF-8 UTF-8\)/\1/' /etc/locale.gen
+sed -i 's/^# *\(ru_RU.UTF-8 UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+update-locale LANG=en_US.UTF-8
+
 [ -f /usr/share/keyrings/foreman.gpg ] && rm -f /usr/share/keyrings/foreman.gpg
 wget -O- https://deb.theforeman.org/pubkey.gpg | gpg --dearmor -o /usr/share/keyrings/foreman.gpg 2>/dev/null || true
 echo "deb [signed-by=/usr/share/keyrings/foreman.gpg] http://deb.theforeman.org/ bookworm 3.17" | tee /etc/apt/sources.list.d/foreman.list
