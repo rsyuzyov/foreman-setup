f55e556d3138bea46984097085553ff56778f7c1
Roman Syuzyov <rsyuzyov@gmail.com>
2025-11-15 07:51:20 +0000

    init

---
+++ foreman-setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+grep -qxF "net.ipv6.conf.all.disable_ipv6 = 1" /etc/sysctl.conf || echo "net.ipv6.conf.all.disable_ipv6 = 1" | tee -a /etc/sysctl.conf
+grep -qxF "net.ipv6.conf.default.disable_ipv6 = 1" /etc/sysctl.conf || echo "net.ipv6.conf.default.disable_ipv6 = 1" | tee -a /etc/sysctl.conf
+grep -qxF "net.ipv6.conf.lo.disable_ipv6 = 1" /etc/sysctl.conf || echo "net.ipv6.conf.lo.disable_ipv6 = 1" | tee -a /etc/sysctl.conf
+
+grep -qxF "127.0.0.1 $(hostname -f) $(hostname -s)" /etc/hosts || echo "127.0.0.1 $(hostname -f) $(hostname -s)" >> /etc/hosts
+
+apt update && apt dist-upgrade -y
+apt install -y ca-certificates wget gnupg lsb-release
+[ -f /usr/share/keyrings/foreman.gpg ] && rm -f /usr/share/keyrings/foreman.gpg
+wget -O- https://deb.theforeman.org/pubkey.gpg | gpg --dearmor -o /usr/share/keyrings/foreman.gpg 2>/dev/null || true
+echo "deb [signed-by=/usr/share/keyrings/foreman.gpg] http://deb.theforeman.org/ bookworm 3.17" | tee /etc/apt/sources.list.d/foreman.list
+echo "deb [signed-by=/usr/share/keyrings/foreman.gpg] http://deb.theforeman.org/ plugins 3.17" | tee /etc/apt/sources.list.d/foreman-plugins.list
+apt update
+
+apt --fix-broken install -y 
+apt install -y openjdk-17-jdk
+apt install -y postgresql
+systemctl enable postgresql
+systemctl start postgresql
+
+# Установка из репы puppet не возможна из-за низкой скорости скачаивания; скачать руками, закинуть в ВМ и установить:
+#https://apt.puppet.com/pool/bookworm/puppet8/p/puppet-agent/puppet-agent_8.10.0-1bookworm_amd64.deb
+#https://apt.puppet.com/pool/bookworm/puppet8/p/puppetserver/puppetserver_8.7.0-1bookworm_all.deb
+dpkg -i ./assets/*.deb || true
+apt --fix-broken install -y 
+
+apt install -y ruby-foreman-fog-proxmox
+apt install -y ruby-foreman-docker
+
+foreman-installer --enable-foreman-plugin-ansible --enable-foreman-proxy-plugin-ansible
+
+echo "Foreman installation completed. Access the web interface at: https://$(hostname -f)"
